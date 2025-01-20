773c5b4fb036802f42197a77eea34fd1d95e9932
ansel <populator@live.com>
2025-01-20 10:16:59 +0500

    development

--- outline_for_denis/settings.py
+++ outline_for_denis/settings.py
@@ -29,6 +29,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 DEBUG = True
 
 ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS').split(',')
+# CSRF_TRUSTED_ORIGINS =os.getenv('DJANGO_CSRF_TRUSTED_ORIGINS').split(',')
 
 # Application definition
 
