91c41b0fc148725e668bb7f64b1add4263f59c8b
ansel <populator@live.com>
2025-01-26 00:07:19 +0500

    auth

--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -2,7 +2,8 @@ from django.contrib.messages.views import SuccessMessageMixin
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
-from bot.models import VpnKey
+from bot.main.timeweb.das import server
+from bot.models import VpnKey, Server, TelegramUser
 
 
 class ProfileView(SuccessMessageMixin, TemplateView):
@@ -10,8 +11,10 @@ class ProfileView(SuccessMessageMixin, TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        context['servers'] = Server.objects.filter(is_active=True).values_list('country__name_for_app', flat=True).distinct()
         try:
-            context['vpn_key'] = VpnKey.objects.get(user=self.request.user.profile.telegram_user)
+            context['vpn_key'] = VpnKey.objects.select_related('user').get(user=self.request.user.profile.telegram_user)
         except VpnKey.DoesNotExist:
             ...
+        context['total_users'] = TelegramUser.objects.count()
         return context
